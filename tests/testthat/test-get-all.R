27583b4ae51ed4ab3133f54bcdb916eb45484d4f
Quinn White <qwhite@smith.edu>
2022-04-11 16:54:15 -0400

    issue #3

---
+++ tests/testthat/test-get-all.R
@@ -0,0 +1,14 @@
+test_that("get_all functions as expected when total elements > 5000", {
+  skip("TIME + RATE CONSUMING, TEST IF MAKING CHANGES TO > 5000 SECTION")
+  key <- Sys.getenv("DATA_GOV_KEY")
+  # skip this test unless you want to use a significant number of requests + are willing to wait
+  url <- paste0("https://api.regulations.gov/v4/comments?filter[commentOnId]=09000064846eebaf&page[size]=250&page[number]=1&sort=lastModifiedDate,documentId&api_key=",
+                key)
+
+  result <- get_all(url, 88061)
+  links <- result[grepl("https://api.regulations.gov/v4/comments", result)]
+
+  expect_equal(length(links), 88061)
+
+
+})
