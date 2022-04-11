00825b4d9b1f70b43fd67e63b9e40e00d1f43e35
Quinn White <qwhite@smith.edu>
2022-04-11 15:52:45 -0400

    issue #4
    if number of comment links is > 500, user will hit API limit. Use purrr::slowly to iterate more slowly to avoid this.

--- R/get-all-comments.R
+++ R/get-all-comments.R
@@ -15,6 +15,10 @@
 #'   \code{\link{set_datagov_key}}. You can use "DEMO_KEY" for a very limited number of calls if needed.
 #'   Obtain a key at [here](https://open.gsa.gov/api/regulationsgov/#getting-started).
 #' @param test logical; to test if the output is as expected without using up many API calls.
+#' @param quiet logical; FALSE if you want to see the progress of the function as it
+#' acquires detailed information for each comment. This is recommended if you are obtaining
+#' more than 500 comments, since this call will take over an hour to run due to rate
+#' limits for the API.
 #' @return a data frame containing the comment metadata, including the download links
 #' @importFrom dplyr bind_rows mutate
 #' @export
@@ -23,7 +27,7 @@
 #' # retrieve all comments for docket CMS-2014-0063
 #' comments_CMS_2014_0063 <- get_all_comments(docketId = "CMS-2014-0063", test = TRUE)
 #' }
-get_all_comments <- function(docketId = NULL, documentId = NULL, test = FALSE, key = NULL) {
+get_all_comments <- function(docketId = NULL, documentId = NULL, test = FALSE, key = NULL, quiet = TRUE) {
   key <- check_auth(key)
   url <- ifelse(!is.null(documentId),
                 construct_document_url(documentId = documentId,
@@ -33,9 +37,7 @@ get_all_comments <- function(docketId = NULL, documentId = NULL, test = FALSE, k
 
   docs <- iterate_over_pages(url)
 
- # objids <- docs$data$attributes.objectId
   objids <- find_element(docs, "objectId")
-  #  objids <- map(docs, ~find_element(.x, "objectId"))
 
 
   # extract all comments for each document; each link corresponds to one document
@@ -55,11 +57,28 @@ get_all_comments <- function(docketId = NULL, documentId = NULL, test = FALSE, k
     comment_links <- comment_links[1:3]
   }
 
+  # if interactive session, check if the user wants to proceed
+  # when number of elements is high and run time will also be high
+
+  # avoid hitting rate limit by slowing iteration if there are more than 500 comments
+  # since rate limit is 500 per hour
+  if (length(comment_links) > 500) {
+    if (interactive()) {
+      continue <- check_continue(length(comment_links))
+      if (!continue) return(NULL)
+    }
+    extract_meta_slow <- purrr::slowly(extract_meta,
+                                       rate = purrr::rate_delay(7.2))
+  }
+  else {
+    extract_meta_slow <- extract_meta
+  }
+
   # retrieve the comment metadata, including download links, for all comments
-  r <- map(comment_links, extract_meta)
+  r <- map(comment_links, ~{if (!quiet) message(.x); extract_meta_slow(.x, key = key);})
 
   result <- tryCatch(
-    do.call("bind_rows", r),
+    bind_rows(r),
 
     error = function(cnd) {
       message(cnd)
@@ -69,6 +88,25 @@ get_all_comments <- function(docketId = NULL, documentId = NULL, test = FALSE, k
   result
 }
 
+
+
+check_continue <-  function(num_elements) {
+  message("There are ",
+                    num_elements,
+                    " elements.\nDue to the rate limits for the regulations.gov API,",
+                    "\nthis will take ",
+                    num_elements/500,
+                    " hours to complete.")
+  response <- readline(prompt = "Do you want to continue (Y/N)? ")
+
+  if (tolower(response) %in% c("y", "yes")) {
+    return(TRUE)
+  }
+  else {
+    message("Exiting the function.")
+    return(FALSE)
+  }
+}
 
 
 
