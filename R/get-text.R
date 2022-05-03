b8744f95f21d052a4d8d3eb673b7d97f84ba5266
Quinn White <qwhite@smith.edu>
2022-05-03 08:46:01 -0400

    style get_text

--- R/get-text.R
+++ R/get-text.R
@@ -12,8 +12,10 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' result <- get_all_documents(documentId = c("FDA-2012-S-1144-0322",
-#' "NHTSA-2008-0060-0827", "DOT-OST-2018-0206-0008", "CMS-2018-0104-0001"))
+#' result <- get_all_documents(documentId = c(
+#'   "FDA-2012-S-1144-0322",
+#'   "NHTSA-2008-0060-0827", "DOT-OST-2018-0206-0008", "CMS-2018-0104-0001"
+#' ))
 #' resul <- add_text(result)
 #' }
 add_text <- function(df, all = FALSE) {
@@ -21,19 +23,22 @@ add_text <- function(df, all = FALSE) {
   # additional packages are needed to run this function, so generate
   # appropriate message to inform the user about packages they need to install
   if (!requireNamespace("textreadr", quietly = TRUE)) {
-    error <- paste0(error, "Install \"textreadr\" to use this function.") }
+    error <- paste0(error, "Install \"textreadr\" to use this function.")
+  }
   if (!requireNamespace("tesseract", quietly = TRUE)) {
-    error <- paste0(error, "Install \"tesseract\" to use this function.") }
+    error <- paste0(error, "Install \"tesseract\" to use this function.")
+  }
 
   if (error != "") {
     stop(error,
-         call. = FALSE)
+      call. = FALSE
+    )
   }
   if (!"fileUrl" %in% names(df)) {
     stop("fileUrl is not a column in the data frame")
   }
   # get text for each file url link
-  text <- purrr::map_chr(df[,"fileUrl"], ~get_text(.x, all))
+  text <- purrr::map_chr(df[, "fileUrl"], ~ get_text(.x, all))
   # add text column to data frame
   df <- cbind(df, text)
   return(df)
@@ -54,16 +59,16 @@ get_text <- function(link, all) {
 
   if (length(links) == 1) {
     text <- extract_text(link)
-  }
-  else {
+  } else {
     if (all) {
       # extract text for all links and collapse this text together
-      text <- purrr::map_chr(links,
-                             extract_text) %>%
+      text <- purrr::map_chr(
+        links,
+        extract_text
+      ) %>%
         paste0(collapse = " ")
       message(class(text))
-    }
-    else {
+    } else {
       # choose best file format
       link <- choose_element(links)
       # extract text for that link
@@ -84,14 +89,11 @@ choose_element <- function(links) {
 
   if (any(docs)) {
     link <- links[which(docs)][1]
-  }
-  else if (any(htm)) {
+  } else if (any(htm)) {
     link <- links[which(htm)][1]
-  }
-  else if (any(pdfs)) {
+  } else if (any(pdfs)) {
     link <- links[which(pdfs)][1]
-  }
-  else {
+  } else {
     link <- links[1]
   }
   return(link)
@@ -103,32 +105,33 @@ choose_element <- function(links) {
 #' @return text extracted from the given download link
 #' @keywords internal
 extract_text <- function(link) {
-  tryCatch( {
-    if (is.na(link)) {
-      txt <- NA
+  tryCatch(
+    {
+      if (is.na(link)) {
+        txt <- NA
+      } else if (!(grepl("pdf", link, fixed = TRUE))) {
+        # read_document can handle multiple formats
+        txt <- textreadr::read_document(link,
+          combine = TRUE
+        )
+      } else {
+        # read pdf with ocr = TRUE method
+        txt <- textreadr::read_pdf(link, ocr = TRUE)[, "text"] %>%
+          paste0(collapse = " ")
+      }
+    },
+    error = function(e) {
+      message(
+        "Attachments for ",
+        link,
+        " could not be obtained."
+      )
+      txt <<- NA
     }
-    else if (!(grepl("pdf", link, fixed = TRUE))) {
-      # read_document can handle multiple formats
-      txt <- textreadr::read_document(link,
-                                       combine = TRUE)
-    }
-    else {
-      # read pdf with ocr = TRUE method
-      txt <- textreadr::read_pdf(link, ocr = TRUE)[,"text"] %>%
-        paste0(collapse = " ")
-    }
-  },
-  error = function(e) {
-    message("Attachments for ",
-            link,
-            " could not be obtained.")
-    txt <<- NA
-  })
+  )
   if (is.null(txt)) {
     return(NA)
-  }
-  else {
+  } else {
     return(txt)
   }
 }
-
